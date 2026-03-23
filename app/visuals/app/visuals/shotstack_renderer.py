f96b5649999f275102414caa83e6d577bd8e54ea
osbornepeter544-a11y <osbornepeter544@gmail.com>
2026-03-23 13:34:41 -0700

    Create shotstack_renderer.py

---
+++ app/visuals/app/visuals/shotstack_renderer.py
@@ -0,0 +1,11 @@
+"""
+Shotstack Renderer Module
+Shotstack integration has been removed from this project.
+"""
+
+
+class ShotstackRenderer:
+
+    @classmethod
+    def render(cls, timeline, case_id, duration):
+        raise Exception("Shotstack integration has been removed. Use the local render engine.")
